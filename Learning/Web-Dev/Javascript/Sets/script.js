2fcc8f36d0d8ff1178d03058b9ce6f7dbe965a04
Salar <141232193+SalarAlo@users.noreply.github.com>
2023-09-25 14:13:54 +0200

    Update script.js

--- Learning/Web-Dev/Javascript/Sets/script.js
+++ Learning/Web-Dev/Javascript/Sets/script.js
@@ -2,14 +2,6 @@
 
 // Destructering
 
-const restaurant = {
-  restaurantName: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-
 
   // ES6 enhanced object literals!
   openingHours: {
