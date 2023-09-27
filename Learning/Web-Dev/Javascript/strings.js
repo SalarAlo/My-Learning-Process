af6c3254da6383f6a1a45a74279b006fc82630a0
Salar <141232193+SalarAlo@users.noreply.github.com>
2023-09-27 18:44:55 +0200

    Update strings.js

--- Learning/Web-Dev/Javascript/strings.js
+++ Learning/Web-Dev/Javascript/strings.js
@@ -12,7 +12,7 @@ airline.length;
 airline.indexOf('r'); // 6 becouse T(0) A(1) P(2)  (3) A(4) I(5) //! R(6)
 
 // we can also get the last index of a charachter
-airline.lastIndexOf('r') // 10
+airline.lastIndexOf('r') // 10 i wont count all of that ngl
 
 // we can slice the string
 // that means that we can call a function wich returns us a substring
@@ -68,6 +68,7 @@ const announcement = 'All passengers come to barding door 23. Barding door 23';
 console.log(announcement.replace("door", 'gate'));
 console.log(priceEU);
 
+
 // Boolean methods
 
 // tells us if a certain substring in another string
@@ -75,3 +76,55 @@ console.log(plane.includes('A3'));
 
 plane.startsWith('A32');
 plane.endsWith('0');
+
+const endsWith = (str, ending) => str.slice(-ending.length) === ending;
+
+console.log(endsWith('Salar', 'ar'));
+
+// We are able to use a certain letter to split up our string into multiple elements of an array
+// imagine this certain letter (also called divider) as a , for the array!
+// in most usecases of this function you have a scentence and you split up each word with a 'space' charachter (' ')
+// then you'll have an array of the words of your scentence
+console.log('a+very+nice+string'.split('+'));
+
+const [firstName, lastName] = 'Salar Alo'.split(' ');
+const calling = 'Mr. ' + lastName.toUpperCase();
+
+// very cool function which takes in 
+const calling2 = ['Mr', firstName, lastName.toUpperCase()].join(' ');
+
+const capitalizeName = function(str){
+  const names = str.split(' ');
+  const namesUpper = [];
+  for(let i = 0; i < names.length; i++)
+  {
+    namesUpper.push(names[i][0].toUpperCase() + names[i].slice(1));
+  }
+
+  return namesUpper.join(' ');
+}
+
+// padding a string
+const message = "Go to gate 23";
+message.padStart(25, '+');
+console.log(message.padEnd(14, '+'));
+
+//! Real wod example of padding
+const maskCreditCard = function(number){
+  const str = String(number);
+  const last = str.slice(-4);
+  const hiddenCreditNumber = last.padStart(str.length, '*');
+  console.log(hiddenCreditNumber);
+  return hiddenCreditNumber;
+}
+
+maskCreditCard('19268198267');
+
+// Repeat
+const message2 = "Bad wather... All Departures Delayed... ";
+const repeatingMessage = message2.repeat(5);
+
+const planesInLine = function(n)
+{
+  console.log('there are ' + n + ' planes in line'.repeat(n));
+}
