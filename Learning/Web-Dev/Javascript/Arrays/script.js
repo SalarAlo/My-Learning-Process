a82e4eccd1e1b0ce0cc3e74ee88ce8f9277e01ae
Salar <141232193+SalarAlo@users.noreply.github.com>
2023-10-01 01:04:20 +0200

    Update script.js

--- Learning/Web-Dev/Javascript/Arrays/script.js
+++ Learning/Web-Dev/Javascript/Arrays/script.js
@@ -61,10 +61,41 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function(movements){
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function(mov, i)
+  {
+    const type = (mov > 0) ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+      <div class="movements__date">3 days ago</div>
+      <div class="movements__value">${mov}</div>
+    </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+
+  });
+}
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // ! LECTURES
 
+
+/*
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+ */
 /* 
 let arr = [...'abcde']; 
 // ! slice
@@ -144,3 +175,18 @@ movements.forEach(function(currElement, i, arr)
   console.log((currElement < 0) ? 'You withdrew ' + Math.abs(currElement) : 'You deposited ' + currElement);
 });
  */
+
+//! Foreach on maps and sets
+/* 
+// Map+
+currencies.forEach(function(value, key, map){
+  console.log(`map[${key}]: ${value}`)
+});
+
+// Set
+
+const movementsUnique = new Set(movements);
+movementsUnique.forEach(function(value, key, map){
+  console.log(value);
+});
+*/
